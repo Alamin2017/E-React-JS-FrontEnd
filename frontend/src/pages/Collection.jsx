534fba84dc7695b0ee5bd621c207d33806d357f6
Al-Amin <alamincse12@gmail.com>
2024-09-21 09:14:44 +0600

    3rd commit

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,14 +1,54 @@
 
 // eslint-disable-next-line no-unused-vars
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets';
 import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
   const {products}=useContext(ShopContext);
   const[showFilter,setShowFilter]=useState(false);
-  console.log(products);
+  const[filterProducts,setFilterProducts]=useState([]);
+
+  const[category,setCategory]=useState([]);
+  const[subCategory,setSubCategory]=useState([]);
+
+  const toggleCategory=(e)=>{
+    if(category.includes(e.target.value)){
+      setCategory(prev=> prev.filter(item => item !== e.target.value));
+    }
+    else{
+      setCategory(prev =>[...prev,e.target.value]);
+    }
+  }
+  const toggleSubCategory=(e)=>{
+    if(subCategory.includes(e.target.value)){
+      setSubCategory(prev=> prev.filter(item => item !== e.target.value));
+    }
+    else{
+      setSubCategory(prev =>[...prev,e.target.value]);
+    }
+  }
+  const applyFilter=()=>{
+    let productsCopy=products.slice();
+    if(category.length>0){
+      productsCopy=productsCopy.filter(item=>category.includes(item.category))
+    }
+    if(subCategory.length>0){
+      productsCopy=productsCopy.filter(item=>subCategory.includes(item.subCategory))
+    }
+    setFilterProducts(productsCopy);
+  }
+
+  useEffect(()=>{
+    setFilterProducts(products);
+  },[products])
+  useEffect(()=>{
+    applyFilter();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[category,subCategory])
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -21,29 +61,29 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Men'}/>Men
+              <input className='w-3' type='checkbox' value={'Men'} onChange={toggleCategory}/>Men
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Women'}/>Women
+              <input className='w-3' type='checkbox' value={'Women'} onChange={toggleCategory}/>Women
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Kids'}/>Kids
+              <input className='w-3' type='checkbox' value={'Kids'} onChange={toggleCategory}/>Kids
             </p>
           </div>
         </div>
 
 
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' :'hidden' } sm:block`}>
-          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Topwear'}/>Topwear
+              <input className='w-3' type='checkbox' value={'Topwear'} onChange={toggleSubCategory}/>Topwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Bottomwear'}/>Bottomwear
+              <input className='w-3' type='checkbox' value={'Bottomwear'} onChange={toggleSubCategory}/>Bottomwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type='checkbox' value={'Winterwear'}/>Winterwear
+              <input className='w-3' type='checkbox' value={'Winterwear'} onChange={toggleSubCategory}/>Winterwear
             </p>
           </div>
         </div>
@@ -52,10 +92,22 @@ const Collection = () => {
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
-          <select>
-            <option></option>
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
           </select>
         </div>
+
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map((item,index)=>(
+              <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+            ))
+          }
+        </div>
+
+
       </div>
 
     </div>
