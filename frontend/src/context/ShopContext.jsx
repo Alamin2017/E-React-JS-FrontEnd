a10c3566951b8dbce835e005cbadd5881b78b61e
Al-Amin <alamincse12@gmail.com>
2024-09-28 12:51:03 +0600

    cart total is added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { createContext, useState } from 'react';
 import { products } from '../assets/assets';
+import { toast } from 'react-toastify';
 
 export const ShopContext = createContext();
 
@@ -9,11 +10,44 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const[search,setSearch]=useState('');
   const[showSearch,setShowSearch]=useState(false);
+  const[cartItems,setCartItems]=useState({});
+  const addToCart=async(itemId,size)=>{
+    if(!size){
+      toast.error('Select Product Size');
+      return;
+    }
+    let cartData=structuredClone(cartItems);
+    if(cartData[itemId]){
+      if(cartData[itemId][size]){
+        cartData[itemId][size] +=1;
+      }
+      else{
+        cartData[itemId][size]=1;
+      }
+    }
+    else{
+      cartData[itemId]={};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  }
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const size in cartItems[items]) {
+        if (cartItems[items][size] > 0) {
+          totalCount += cartItems[items][size];
+        }
+      }
+    }
+    return totalCount;
+  };
+
 
   const value = {
     products,
     currency,
-    delivery_fee,search,setSearch,showSearch,setShowSearch
+    delivery_fee,search,setSearch,showSearch,setShowSearch,cartItems,addToCart,getCartCount
   };
 
   return (
